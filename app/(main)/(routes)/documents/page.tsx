9c56c520644ed9568f63f7160aa49a93497fdc2f
albert19_ <albertnanda1902@gmail.com>
2024-01-02 16:19:30 +0800

    update direktori

---
+++ app/(main)/(routes)/documents/page.tsx
@@ -0,0 +1,7 @@
+const Documents = () => {
+    return (
+        <div>This is restricted</div>
+    );
+}
+
+export default Documents;
--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -3,8 +3,9 @@ import Heading from "./_components/Heading";
 import Heroes from "./_components/Heroes";
 
 const Marketing = () => {
+
   return (
-    <div className="min-h-full flex flex-col dark:bg-[#1F1F1F]">
+    <div className="min-h-full flex flex-col dark:bg-[#1F1F1F]" suppressHydrationWarning={true}>
       <div className="flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-6 pb-10">
         <Heading />
         <Heroes />

f0fb9e1c8df3b34677c9b9f18c57970cf27a76c7
albert19_ <albertnanda1902@gmail.com>
2024-01-02 18:56:52 +0800

    create convex schema

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 import { ThemeProvider } from '@/components/providers/theme-provider'
 import { ConvexClientProvider } from '@/components/providers/convex-provider'
+import { Toaster } from "sonner"
 
 export const metadata: Metadata = {
   title: 'AlNote',
@@ -32,6 +33,7 @@ export default function RootLayout({
       <body>
         <ConvexClientProvider>
           <ThemeProvider attribute='class' defaultTheme='system' enableSystem disableTransitionOnChange storageKey='alnote-theme'>
+            <Toaster position='bottom-center' />
             {children}
           </ThemeProvider>
         </ConvexClientProvider>

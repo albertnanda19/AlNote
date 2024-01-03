517eb37b7c992dd2b13a0f90adca2bf22436d063
albert19_ <albertnanda1902@gmail.com>
2024-01-03 18:59:16 +0800

    create add a page item on sidebar and install shadcn popover component

--- app/(main)/_components/Navigation.tsx
+++ app/(main)/_components/Navigation.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { cn } from "@/lib/utils";
-import { ChevronsLeft, MenuIcon, PlusCircle, Search, Settings } from "lucide-react";
+import { ChevronsLeft, MenuIcon, Plus, PlusCircle, Search, Settings } from "lucide-react";
 import { usePathname } from "next/navigation";
 import { ElementRef, useEffect, useRef, useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
@@ -124,6 +124,7 @@ const Navigation = () => {
                 </div>
                 <div className="mt-4">
                     <DocumentList />
+                    <Item onClick={handleCreate} label="Add a page" icon={Plus} />
                 </div>
                 <div
                     onMouseDown={handleMouseDown}

c10f0a640ad5800c5e8b66751d0880bd6fd51b0b
albert19_ <albertnanda1902@gmail.com>
2024-01-06 18:45:29 +0800

    create menu item component on note navbar

--- app/(main)/_components/Navbar.tsx
+++ app/(main)/_components/Navbar.tsx
@@ -7,6 +7,7 @@ import { MenuIcon } from "lucide-react";
 import { useParams } from "next/navigation";
 import Title from "./Title";
 import Banner from "./Banner";
+import Menu from "./Menu";
 
 interface NavbarProps {
     isCollapsed: boolean;
@@ -25,8 +26,11 @@ const Navbar = ({
 
     if (document === undefined) {
         return (
-            <nav className="bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full flex items-center">
+            <nav className="bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full flex items-center justify-between">
                 <Title.Skeleton />
+                <div className="flex items-center gap-x-2">
+                    <Menu.Skeleton />
+                </div>
             </nav>
         )
     }
@@ -43,6 +47,9 @@ const Navbar = ({
                 )}
                 <div className="flex items-center justify-between w-full">
                     <Title initialData={document} />
+                    <div className="flex items-center gap-x-2">
+                        <Menu documentId={document._id} />
+                    </div>
                 </div>
             </nav>
             {document.isArchived && (
